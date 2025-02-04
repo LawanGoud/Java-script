cdb510e18d4e27e732819d1fc237628952a9a22a
LawanGoud <lavan.bairi@gmail.com>
2025-02-04 15:03:18 +0530

    Sizing of an Image

--- CodingPractice/Sizing an Image/index.js
+++ CodingPractice/Sizing an Image/index.js
@@ -8,16 +8,30 @@ dogImage.style.width = originalImageWidth + "px";
 let imageWidth = document.getElementById("imageWidth");
 imageWidth.textContent = originalImageWidth + "px";
 
+let warningMessage = document.getElementById("warningMessage");
+let maxWidthWarningMessage = "Too big. Decrease the size of the Image";
+let minWidthWarningMessage = "Can't Visible. Increase the size of the Image";
+
 function onIncrement() {
-  originalImageWidth = originalImageWidth + 5;
-  let newWidth = originalImageWidth + "px";
-  dogImage.style.width = newWidth;
-  imageWidth.textContent = newWidth;
+  if (originalImageWidth >= maxImageWidth) {
+    warningMessage.textContent = maxWidthWarningMessage;
+  } else {
+    originalImageWidth = originalImageWidth + 5;
+    let newWidth = originalImageWidth + "px";
+    warningMessage.textContent = "";
+    dogImage.style.width = newWidth;
+    imageWidth.textContent = newWidth;
+  }
 }
 
 function onDecrement() {
-  originalImageWidth = originalImageWidth - 5;
-  let newWidth = originalImageWidth + "px";
-  dogImage.style.width = newWidth;
-  imageWidth.textContent = newWidth;
+  if (originalImageWidth <= minImageWidth) {
+    warningMessage.textContent = minWidthWarningMessage;
+  } else {
+    originalImageWidth = originalImageWidth - 5;
+    let newWidth = originalImageWidth + "px";
+    warningMessage.textContent = "";
+    dogImage.style.width = newWidth;
+    imageWidth.textContent = newWidth;
+  }
 }
