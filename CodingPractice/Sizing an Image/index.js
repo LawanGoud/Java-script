57374495b9b429322f5290abfcd4e02e960792d7
LawanGoud <lavan.bairi@gmail.com>
2025-02-04 14:50:41 +0530

    Added Events

--- CodingPractice/Sizing an Image/index.js
+++ CodingPractice/Sizing an Image/index.js
@@ -7,3 +7,17 @@ dogImage.style.width = originalImageWidth + "px";
 
 let imageWidth = document.getElementById("imageWidth");
 imageWidth.textContent = originalImageWidth + "px";
+
+function onIncrement() {
+  originalImageWidth = originalImageWidth + 5;
+  let newWidth = originalImageWidth + "px";
+  dogImage.style.width = newWidth;
+  imageWidth.textContent = newWidth;
+}
+
+function onDecrement() {
+  originalImageWidth = originalImageWidth - 5;
+  let newWidth = originalImageWidth + "px";
+  dogImage.style.width = newWidth;
+  imageWidth.textContent = newWidth;
+}
