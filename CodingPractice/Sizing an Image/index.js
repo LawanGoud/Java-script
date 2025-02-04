48d93b6c52ba54144068599214312c7af47993f8
LawanGoud <lavan.bairi@gmail.com>
2025-02-04 14:37:44 +0530

    Set Width of Image Using JS

---
+++ CodingPractice/Sizing an Image/index.js
@@ -0,0 +1,9 @@
+let minImageWidth = 100;
+let maxImageWidth = 300;
+let originalImageWidth = 200;
+
+let dogImage = document.getElementById("dogImage");
+dogImage.style.width = originalImageWidth + "px";
+
+let imageWidth = document.getElementById("imageWidth");
+imageWidth.textContent = originalImageWidth + "px";
