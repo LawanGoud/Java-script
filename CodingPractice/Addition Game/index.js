0828f07990021e072d88e00bb37dd59ffd7264a0
LawanGoud <lavan.bairi@gmail.com>
2025-02-05 13:19:19 +0530

    Addition Game

--- CodingPractice/Addition Game/index.js
+++ CodingPractice/Addition Game/index.js
@@ -1,8 +1,33 @@
-let firstNumber = document.getElementById("firstNumber");
-let secondNumber = document.getElementById("secondNumber");
+let firstNumberElement = document.getElementById("firstNumber");
+let secondNumberElement = document.getElementById("secondNumber");
+let userInput = document.getElementById("userInput");
+let gameResult = document.getElementById("gameResult");
+let successMessage = "Congratulations! You got it right!";
+let tryAgainMessage = "Please, Try Again!";
 
-let firstRandomNumber = Math.ceil(Math.random() * 100);
-let secondRandomNumber = Math.ceil(Math.random() * 100);
+firstNumberElement.textContent = Math.ceil(Math.random() * 100);
+secondNumberElement.textContent = Math.ceil(Math.random() * 100);
 
-firstNumber.textContent = firstRandomNumber;
-secondNumber.textContent = secondRandomNumber;
+function checkResult() {
+  let firstNumber = parseInt(firstNumberElement.textContent);
+  let secondNumber = parseInt(secondNumberElement.textContent);
+  let userAnswer = parseInt(userInput.value);
+  let sumOfTwoIntegers = firstNumber + secondNumber;
+
+  if (userAnswer === sumOfTwoIntegers) {
+    gameResult.textContent = successMessage;
+    gameResult.style.backgroundColor = "#028a0f";
+  } else {
+    gameResult.textContent = tryAgainMessage;
+    gameResult.style.backgroundColor = "#1e217c";
+  }
+}
+
+function resetGame() {
+  let firstRandomNumber = Math.ceil(Math.random() * 100);
+  let secondRandomNumber = Math.ceil(Math.random() * 100);
+  firstNumber.textContent = firstRandomNumber;
+  secondNumber.textContent = secondRandomNumber;
+  userInput.value = "";
+  gameResult.textContent = "";
+}
