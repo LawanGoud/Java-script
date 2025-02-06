7d92160481f4a64b71fed7ac3532f92602f1abb5
LawanGoud <lavan.bairi@gmail.com>
2025-02-06 19:48:44 +0530

    Create & Log an Object

---
+++ CodingPractice/Arrays And Objects/Create & Log an Object/index.js
@@ -0,0 +1,21 @@
+// Create & Log an Object
+
+// Write a JS program to log a vechile object with the given properties (key-value pairs).
+
+/*
+name - Mercedes Benz
+model - C - Class
+manufacturer - Daimler AG company
+length = 4686mm
+cargo capacity - 12.6 cubic feet
+*/
+
+let vechile = {
+  name: "Mercedes Benz",
+  model: "C - Class",
+  manufacturer: "Daimler AG company",
+  length: "4686mm",
+  "cargo Capacity": "12.6 cubic feet",
+};
+
+console.log(vechile);
