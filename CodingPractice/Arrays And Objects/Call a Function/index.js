c4b0c12000326b4d12a743442610377d22bc05bb
LawanGoud <lavan.bairi@gmail.com>
2025-02-06 13:24:26 +0530

    Call a Function

---
+++ CodingPractice/Arrays And Objects/Call a Function/index.js
@@ -0,0 +1,11 @@
+// Call a Function
+
+// Given a function (greet) in the prefilled code.
+// Write a JS code to call the given function and log it.
+
+function greet() {
+  return "Hello! Have a nice day";
+}
+
+let greetings = greet();
+console.log(greetings);
