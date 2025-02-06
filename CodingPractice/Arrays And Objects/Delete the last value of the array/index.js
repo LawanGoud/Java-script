bf7828252754e4fcc2465f75de3a8942d300caee
LawanGoud <lavan.bairi@gmail.com>
2025-02-06 13:19:47 +0530

    Delete the last value of the array

---
+++ CodingPractice/Arrays And Objects/Delete the last value of the array/index.js
@@ -0,0 +1,42 @@
+// Delete the last value of the array
+
+//Given an array (myArray)
+// Write a JS program to delete the last value of the array using the array method pop and log the deleted value and updated array.
+
+"use strict";
+
+process.stdin.resume();
+process.stdin.setEncoding("utf-8");
+
+let inputString = "";
+let currentLine = 0;
+
+process.stdin.on("data", (inputStdin) => {
+  inputString += inputStdin;
+});
+
+process.stdin.on("end", (_) => {
+  inputString = inputString
+    .trim()
+    .split("\n")
+    .map((str) => str.trim());
+
+  main();
+});
+
+function readLine() {
+  return inputString[currentLine++];
+}
+
+function main() {
+  let input = readLine().replace(/'/g, '"');
+  // myArray
+  let myArray = JSON.parse(input);
+
+  /*
+   * Write your code here and log the output.
+   */
+  let deletedVal = myArray.pop();
+  console.log(deletedVal);
+  console.log(myArray);
+}
